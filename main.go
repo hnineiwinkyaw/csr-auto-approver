aefb54c890f5ef06635de6f51aa80cadc3cad0f2
Hnin Ei Win Kyaw <hnineiwinkyaw.ycc@gmail.com>
2021-08-12 12:55:24 +0630

    fix: validate pod ip before approve

--- main.go
+++ main.go
@@ -6,25 +6,92 @@ import (
 	"fmt"
 	"path/filepath"
 	"time"
+	"strings"
 
 	"k8s.io/client-go/kubernetes"
 	certs "k8s.io/client-go/kubernetes/typed/certificates/v1beta1"
 	certificate "k8s.io/api/certificates/v1beta1"
-	core "k8s.io/client-go/kubernetes/typed/core/v1"
+	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	// "log"
 
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+
+	"encoding/pem"
+	"crypto/x509"
 )
 
-func approveCsrs(csr certs.CertificateSigningRequestInterface, pod core.PodInterface) bool {
-	return true
+func getCertificateCommonName(csrObj *certificate.CertificateSigningRequest) string {
+	block, _ := pem.Decode(csrObj.Spec.Request)
+
+	if block == nil || block.Type != "CERTIFICATE REQUEST" {
+		fmt.Printf(block.Type)
+		fmt.Printf("PEM block type must be CERTIFICATE REQUEST")
+	}
+	csr, err := x509.ParseCertificateRequest(block.Bytes)
+	if err != nil {
+		fmt.Printf("Error in parsing cr")
+	}
+
+	// fmt.Printf("%+v\n",csr.IPAddresses)
+
+	return csr.Subject.CommonName
 }
 
-type Message struct {
-    Type string
-    Reason string
+func extractCSRStatus(csr *certificate.CertificateSigningRequest) (string, error) {
+	var approved, denied bool
+	for _, c := range csr.Status.Conditions {
+		switch c.Type {
+		case certificate.CertificateApproved:
+			approved = true
+		case certificate.CertificateDenied:
+			denied = true
+		default:
+			return "", fmt.Errorf("unknown csr condition %q", c)
+		}
+	}
+	var status string
+	// must be in order of presidence
+	if denied {
+		status += "Denied"
+	} else if approved {
+		status += "Approved"
+	} else {
+		status += "Pending"
+	}
+	if len(csr.Status.Certificate) > 0 {
+		status += ",Issued"
+	}
+	return status, nil
+}
+
+func doesCnResolvesIpAddr(commonName string, pods *core.PodList) bool {
+
+	if  !strings.HasSuffix(commonName, ".pod.cluster.local") {
+		fmt.Println("No Suffix with pod.cluster.local")
+		return false
+	}
+
+	podIpPlusNs := strings.Split(commonName,".pod.cluster.local")[0]
+
+	fmt.Printf("Pod IP and namespace : %s \n", podIpPlusNs)
+
+	for _, pod := range pods.Items {
+		
+		fmt.Println("Filtering by namespace\n")
+
+		if  strings.HasSuffix(podIpPlusNs, pod.Namespace) {
+			fmt.Printf("Pod found with ns : %s, getting pod ip ... \n", pod.Namespace)
+			podIp := strings.Split(podIpPlusNs,"."+pod.Namespace)
+			fmt.Printf("Pod IP : %s \n", podIp[0])
+			fmt.Printf("Filtering actual pod by certificate ca podIp ... \n")
+			if podIp[0] == pod.Status.PodIP {
+				fmt.Printf("Found Pod : %s in namespace %s" , pod.Name, pod.Namespace)
+				return true;
+			}
+		}
+	}
+	return false
 }
 
 func main() {
@@ -67,34 +134,47 @@ func main() {
 		
 		fmt.Printf("There are %d csr in the cluster\n", len(csrs.Items))
 
-		for i, csrObj := range csrs.Items {
-			fmt.Println(i)
-		    fmt.Println(csrObj.Name)
-		    fmt.Printf("%+v\n",csrObj.Status.Conditions)
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(),metav1.ListOptions{})
+		if err != nil {
+			fmt.Println("Error in listing pods")
+		}
 
-		    if len(csrObj.Status.Conditions) > 0 && csrObj.Status.Conditions[0].Type == certificate.CertificateApproved {
-		    	fmt.Printf("Already approved")
-		    } else {
-		    	csrObj.Status.Conditions = append(csrObj.Status.Conditions, certificate.CertificateSigningRequestCondition{
-					Type:			certificate.CertificateApproved,
-					Reason: 		"handle by csr auto approver",
-					Message:		"This CSR was approved by csr-approver-app",
-					LastUpdateTime: metav1.Now(),
-				})
 
-			    fmt.Printf("%+v\n", csrObj.Status.Conditions)
+		for _, csrObj := range csrs.Items {
 
-			    clientset.CertificatesV1beta1().CertificateSigningRequests().UpdateApproval(context.TODO(),&csrObj,metav1.UpdateOptions{})
+			status, err := extractCSRStatus(&csrObj)
+
+			if err != nil {
+				panic(err.Error())
+			}
+
+			fmt.Printf("Certificate status : %s \n", status)
+
+		    if strings.HasPrefix(status, "Pending") {
+
+		    	commonName := getCertificateCommonName(&csrObj)
+
+				fmt.Printf("Common Name is %s \n", commonName)
+
+			    validPod := doesCnResolvesIpAddr(commonName, pods)			
+
+			    if validPod {
+			    	fmt.Printf("Approving Certificate ... \n")
+			    	csrObj.Status.Conditions = append(csrObj.Status.Conditions, certificate.CertificateSigningRequestCondition{
+						Type:			certificate.CertificateApproved,
+						Reason: 		"handled by csr auto approver",
+						Message:		"This CSR was approved by csr-approver-app",
+						LastUpdateTime: metav1.Now(),
+					})
+
+					clientset.CertificatesV1beta1().CertificateSigningRequests().UpdateApproval(context.TODO(),&csrObj,metav1.UpdateOptions{})
+			    }
+		    	
 		    }
-		    
-		    fmt.Printf("\n\n\n\n")
 		}
 		
-		// FIXME: only working in the default namespace
-		// pod := clientset.CoreV1().Pods("default")
+		fmt.Printf("Sleeping for 10 second ... \n\n")
 
-		// log.Fatalln(approveCsrs(csr, pod))
-
-		time.Sleep(50 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 }
